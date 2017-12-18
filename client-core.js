6f1719871e1375b2f074a4a8c59896ae572d55b8
Александр <sanya.rogov37@gmail.com>
2017-12-18 21:03:14 +0500

    Final

--- client-core.js
+++ client-core.js
@@ -3,13 +3,13 @@
 module.exports.execute = execute;
 module.exports.isStar = false;
 
-const supportedKeys = ['--from', '--to', '--text'];
 const rp = require('request-promise');
 const chalk = require('chalk');
+const getArgs = require('minimist');
 
 function execute() {
-    const args = process.argv.slice(2);
-    const { action, from, to, text } = processArgs(args);
+    const action = process.argv[2];
+    const { from, to, text } = getArgs(process.argv.slice(3));
 
     if (!action) {
         return Promise.reject('Не указана команда');
@@ -30,41 +30,17 @@ function execute() {
     };
 
     return rp(options)
-        .then(body => forFancyPrint(body));
+        .then(body => {
+            if (body instanceof Array) {
+                return body.map(forFancyPrint).join('\n\n');
+            }
+
+            return forFancyPrint(body);
+        });
 }
 
-function processArgs(args) {
-    let paramObject = { action: args[0] };
-    let i = 1;
-    while (i < args.length) {
-        let param = args[i];
-        let [key, value] = param.split('=');
-        if (supportedKeys.includes(key) && value === undefined && args[i + 1]) {
-            paramObject[key.slice(2)] = args[i + 1];
-            i = i + 2;
-            continue;
-        }
-        if (supportedKeys.includes(key) && value) {
-            paramObject[key.slice(2)] = value;
-            i++;
-            continue;
-        }
-        i = args[i + 1] ? i + 1 : i + 2;
-    }
-
-    return paramObject;
-}
-
-function forFancyPrint(messages) {
-    if (messages instanceof Array) {
-        return messages.map(message => {
-            return (message.from ? `${chalk.hex('#f00')('FROM')}: ${message.from}\n` : '') +
-                (message.to ? `${chalk.hex('#f00')('TO')}: ${message.to}\n` : '') +
-                (message.text ? `${chalk.hex('#0f0')('TEXT')}: ${message.text}` : '');
-        }).join('\n\n');
-    }
-
-    return (messages.from ? `${chalk.hex('#f00')('FROM')}: ${messages.from}\n` : '') +
-        (messages.to ? `${chalk.hex('#f00')('TO')}: ${messages.to}\n` : '') +
-        (messages.text ? `${chalk.hex('#0f0')('TEXT')}: ${messages.text}` : '');
+function forFancyPrint(message) {
+    return (message.from ? `${chalk.hex('#f00')('FROM')}: ${message.from}\n` : '') +
+        (message.to ? `${chalk.hex('#f00')('TO')}: ${message.to}\n` : '') +
+        (message.text ? `${chalk.hex('#0f0')('TEXT')}: ${message.text}` : '');
 }
--- server-core.js
+++ server-core.js
@@ -8,52 +8,48 @@ let storedMessages = [];
 
 const server = http.createServer((req, res) => {
     const urlObj = parseUrl(req.url);
-    if (urlObj.pathname === '/messages') {
-        const { from, to } = parseQuery(urlObj.query);
-
-        if (req.method === 'GET') {
-            res.setHeader('Content-Type', 'application/json');
-            res.write(JSON.stringify(getMessages(from, to, storedMessages)));
-            res.end();
-        }
-
-        if (req.method === 'POST') {
-            let body = '';
-            req.on('data', (chunk) => {
-                body += chunk;
-            }).on('end', () => {
-                let message = formatMessage(from, to, JSON.parse(body).text);
-
-                storedMessages.push(message);
-                res.setHeader('Content-Type', 'application/json');
-                res.write(JSON.stringify(message));
-                res.end();
-            });
-        }
-    } else {
+    if (urlObj.pathname !== '/messages') {
         res.statusCode = 404;
         res.end();
+
+        return;
+    }
+    const { from, to } = parseQuery(urlObj.query);
+
+    if (req.method === 'GET') {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(getMessages(from, to, storedMessages)));
+    }
+
+    if (req.method === 'POST') {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk;
+        }).on('end', () => {
+            let message = formatMessage({ from, to, text: JSON.parse(body).text });
+
+            storedMessages.push(message);
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(message));
+        });
     }
 });
 
 function getMessages(from, to, messageList) {
-    return messageList.filter(function (message) {
+    return messageList.filter(message => {
         return (from ? message.from === from : true) && (to ? message.to === to : true);
     });
 }
 
-function formatMessage(from, to, text) {
-    if (!from && !to) {
-        return { text };
-    }
-    if (!from) {
-        return { to, text };
-    }
-    if (!to) {
-        return { from, text };
+function formatMessage(messageObj) {
+    let formattedMessage = {};
+    for (let prop of Object.keys(messageObj)) {
+        if (messageObj[prop]) {
+            formattedMessage[prop] = messageObj[prop];
+        }
     }
 
-    return { from, to, text };
+    return formattedMessage;
 }
 
 module.exports = server;
